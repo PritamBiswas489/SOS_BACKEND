e49f4db4e1f18efc9c6360b1b3f5bdeb9d9d9d97
PritamBiswas489 <pritam.biswas489@gmail.com>
2026-03-19 06:55:06 +0530

    190320260654AM

--- src/swagger.js
+++ src/swagger.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Travel Wallet API",
+      title: "SOS alert API",
       version: "1.0.0",
       description: "API docs with Swagger for travel wallet",
     },
