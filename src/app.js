3236ad0b38705dbcdb0808ada18631fec088879c
SAPTARSHA SAHA <aqualeaf_mac_1@SAPTARSHAs-Mac-mini.local>
2026-06-25 22:26:50 +0530

    25jun20261026pm

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
+import "../instrument.js";  
 import "./config/environment.js";
-
 import express, { Router, json, urlencoded } from "express";
 import compression from "compression";
 import cors from "cors";
@@ -18,7 +18,7 @@ import multer from "multer";
 import customReturn from "./middlewares/responseBuilder.js";
 import locales from "./middlewares/locales.js";
 import { PROFILE_IMAGE_SIZE_ERROR_MESSAGE } from "./middlewares/profileImageUpload.js";
-import { initializeSentry } from "./config/sentry.config.js";
+// import { initializeSentry } from "./config/sentry.config.js";
 // import "./cron/index.js"
 import cookieParser from "cookie-parser";
 import logger from "./config/winston.js";
@@ -45,6 +45,7 @@ const publicDir =
     ? pathResolve(pathJoin(dirname("./"), "public"))
     : pathResolve(pathJoin(dirname("./"), "public"));
 
+  
 const app = express();
 
 app.use((req, res, next) => {
@@ -78,11 +79,11 @@ app.use((req, res, next) => {
   next();
 });
 
-if (SENTRY_ENABLED === "true") {
-  (async () => {
-    await initializeSentry(SENTRY_DSN);
-  })();
-}
+// if (SENTRY_ENABLED === "true") {
+//   (async () => {
+//     await initializeSentry(SENTRY_DSN);
+//   })();
+// }
 app.use(
   cors({
     origin: [
